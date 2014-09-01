94bd0975837f6c4cb34e8041f099fc72681825c3
CarstenK <CarstenK@posteo.de>
2014-09-02 00:28:38 +0200

    corrected gpl comment

--- src/colsum.cpp
+++ src/colsum.cpp
@@ -8,20 +8,20 @@
  *   Email CarstenK[at]posteo.de
  *
  *
- * This file is part of LittleToolBox.
+ * This file is part of ColumnFileHelpers.
  *
- * LittleToolBox is free software: you can redistribute it and/or modify
+ * ColumnFileHelpers is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * LittleToolBox is distributed in the hope that it will be useful,
+ * ColumnFileHelpers is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with LittleToolBox.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ColumnFileHelpers.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
